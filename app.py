b40a6e57dff8f68b0850bab3c6f94f200c147e86
Neal Paresh Patel <nealpareshpatel@gmail.com>
2023-03-06 16:51:42 -0500

    request working to make etf_returns call

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
+from indexcalculation import etf_returns
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/result', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/result", methods=["POST"])
 def result():
-    if request.method == 'POST':
-        name = request.form['name']
+    result = request.form.to_dict(flat=False)
+    print(
+        etf_returns(
+            {
+                result["symbol"][i]: result["weight"][i]
+                for i in range(len(result["symbol"]))
+            }
+        )
+    )
+    return render_template("index.html")
+
+
+#    if request.method == 'POST':
+#        name = request.form['name']
 
 if __name__ == "__main__":
 
