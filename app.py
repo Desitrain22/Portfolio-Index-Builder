be84f1c10d85e416187e5db4124fe28c544b8124
Neal Paresh Patel <59032397+Desitrain22@users.noreply.github.com>
2023-03-17 01:01:18 -0400

    added default sample values

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect
+import random
 import pandas as pd
 from indexcalculation import etf_returns
 
@@ -7,7 +8,40 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    samples = [
+        [
+            "MANGA Index",
+            ("META", 0.2),
+            ("AAPL", 0.2),
+            ("NFLX", 0.2),
+            ("GOOG", 0.2),
+            ("AMZN", 0.2),
+        ],
+        [
+            "Becky Index",
+            ("LULU", 25),
+            ("ADBE", 15),
+            ("SBUX", 35),
+            ("AAPL", 20),
+            ("PTON", 5),
+        ],
+        [
+            "Symbolz",
+            ("PZZA", 3),
+            ("YUM", 3),
+            ("SHOO", 10),
+            ("SIX", 3),
+            ("ROCK", 3),
+            ("LUV", 10),
+            ("JOB", 10),
+            ("GLUU", 3),
+            ("FROG", 5),
+            ("COKE", 10),
+            ("BBW", 69),
+        ],
+    ]
+    set = samples[random.randint(0, len(samples) - 1)]
+    return render_template("index.html", name=set[0], default=set[1:])
 
 
 @app.route("/result", methods=["POST"])
@@ -23,7 +57,8 @@ def result():
             }
         )
     )
-    return render_template("index.html")
+    return index()
+    # return redirect('/')  # render_template("index.html")
 
 
 if __name__ == "__main__":
